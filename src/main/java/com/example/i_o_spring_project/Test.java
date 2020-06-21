a4d898d2c0c4b43a2b43cac9a9669b52e343ae8b
OrShemesh <orshemesh5020@gmail.com>
2020-06-21 10:02:41 +0300

    bla bla 2

---
+++ src/main/java/com/example/i_o_spring_project/Test.java
@@ -0,0 +1,12 @@
+package com.example.i_o_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(IOSpringProjectApplication.class,
+				args);
+	}
+}
