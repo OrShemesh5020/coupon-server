e3f62e61abce7779f979c0e7caf4f0fa83119801
OrShemesh <orshemesh5020@gmail.com>
2020-09-23 22:06:23 +0300

    adding 'removePurchasedCoupon' and 'getCoupon' functions.
    changing @GetMapping to the 'getCustomerCoupons' (by price and category) functions.
    adding @PutMapping to 'updateCustomerDetails' function

--- src/main/java/com/example/i_o_spring_project/controller/CustomerController.java
+++ src/main/java/com/example/i_o_spring_project/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -35,23 +36,29 @@ public class CustomerController extends ClientController {
 		return new ResponseEntity<Coupon>(customerService.purchaseCoupon(coupon), HttpStatus.OK);
 	}
 
-	@DeleteMapping
-	public void name() {
-
+	@DeleteMapping("/coupon/{id}")
+	public ResponseEntity<Void> removePurchasedCoupon(@PathVariable int id) {
+		customerService.removeCouponPurchase(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 
+	@GetMapping("/coupon")
+	public ResponseEntity<Coupon> getCoupon(int id) {
+		return new ResponseEntity<Coupon>(customerService.getOneCoupon(id), HttpStatus.OK);
+	}
+	
 	@GetMapping("/coupons")
 	public ResponseEntity<List<Coupon>> getCustomerCoupons() {
 		return new ResponseEntity<List<Coupon>>(customerService.getCustomerCoupons(), HttpStatus.OK);
 	}
 
-	@GetMapping("/coupons/{categoryId}")
+	@GetMapping("/coupons/categoryId/{categoryId}")
 	public ResponseEntity<List<Coupon>> getCustomerCoupons(@PathVariable Integer categoryId) {
 		Category category = companyService.getCategory(categoryId);
 		return new ResponseEntity<List<Coupon>>(customerService.getCustomerCoupons(category), HttpStatus.OK);
 	}
 
-	@GetMapping("/coupons/{price}")
+	@GetMapping("/coupons/price/{price}")
 	public ResponseEntity<List<Coupon>> getCustomerCoupons(@PathVariable Double price) {
 		return new ResponseEntity<List<Coupon>>(customerService.getCustomerCoupons(price), HttpStatus.OK);
 	}
@@ -61,6 +68,7 @@ public class CustomerController extends ClientController {
 		return new ResponseEntity<Customer>(customerService.getCustomerDetails(), HttpStatus.OK);
 	}
 
+	@PutMapping("/details")
 	public ResponseEntity<Customer> updateCustomerDetails(@RequestBody Customer customer) {
 		return new ResponseEntity<Customer>(customerService.UpdateDetails(customer), HttpStatus.OK);
 	}
