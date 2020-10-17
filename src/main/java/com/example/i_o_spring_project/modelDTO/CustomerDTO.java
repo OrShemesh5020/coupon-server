894923c3ffba111a37a94b2bf84f6163cf95fd48
OrShemesh <orshemesh5020@gmail.com>
2020-10-17 22:59:18 +0300

    add @component to CompanyDTO and CustomerDTO

--- src/main/java/com/example/i_o_spring_project/modelDTO/CustomerDTO.java
+++ src/main/java/com/example/i_o_spring_project/modelDTO/CustomerDTO.java
@@ -1,5 +1,7 @@
 package com.example.i_o_spring_project.modelDTO;
 
+import org.springframework.stereotype.Component;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Component
 public class CustomerDTO {
 
 	private Integer id;
